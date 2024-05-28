866eac57665053b88bd6d64f5ecc69c9a1fbc65e
minhquantb00 <minhquantb00@gmail.com>
2024-05-28 20:33:56 +0700

    init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+import Antd from 'ant-design-vue'
+import CKEditor from '@ckeditor/ckeditor5-vue'
+import eventBus from './plugins/eventBus'
 
 loadFonts();
-
-createApp(App).use(router).use(store).use(vuetify).mount("#app");
+const app = createApp(App)
+app.use(eventBus)
+app.use(Antd)
+app.use(CKEditor)
+app.use(router)
+app.use(store)
+app.use(vuetify)
+app.mount("#app")
